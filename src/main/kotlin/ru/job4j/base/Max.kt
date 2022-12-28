a503b8b833e4981e52f7e9be524a085b9c717cb3
oleg <waterpoleg@gmail.com>
2022-12-28 20:55:25 +0300

    4. Операторы условий if [#189221 #375364]

--- src/main/kotlin/ru/job4j/base/Max.kt
+++ src/main/kotlin/ru/job4j/base/Max.kt
@@ -2,6 +2,5 @@ package ru.job4j.base
 
 fun max(first: Int, second: Int) = if (first > second) first else second
 
-fun maxFromThree(first: Int, second: Int, third: Int): Int {
-    return if (first > second) max(first, third) else max(second, third)
-}
+fun maxFromThree(first: Int, second: Int, third: Int) = max(first, max(second, third))
+
